88410033bb20a65bab7cd8b317f59b01ce4622f8
dvegas03_Desktop <khardan100@gmail.com>
2025-07-02 18:06:15 -0700

    Initialize backend

---
+++ backend/src/main/java/com/chefscircle/backend/ChefscircleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chefscircle.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefscircleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefscircleBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/chefscircle/backend/ChefscircleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chefscircle.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefscircleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
